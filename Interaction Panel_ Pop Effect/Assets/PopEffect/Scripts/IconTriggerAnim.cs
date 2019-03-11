b9989f50b50b6187cd9d608cae331d5842b889c3
Phu-Phat-Félix TRUONG <p.truong@etudiant.ecolemelies.fr>
2019-03-11 12:29:58 +0100

    Add Interaction Panel _Popeffect

---
+++ Interaction Panel_ Pop Effect/Assets/PopEffect/Scripts/IconTriggerAnim.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class IconTriggerAnim : MonoBehaviour
+{
+    [Header("Pop Controller")]
+    public Animator animLook_In;
+    public Animator animUse_In;
+    public Animator animTalk_In;
+    public Animator animTake_In;
+
+    ///Pop In Animations Only///
+    public void LookAnimationEnter()
+    {
+        Debug.Log("PointEnter is Triggered");
+        animLook_In.SetBool("IsHighlight", true);
+    }
+    public void LookAnimationExit()
+    {
+        Debug.Log("ExitEnter");
+        animLook_In.SetBool("IsHighlight", false);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public void TakeAnimationEnter()
+    {
+        Debug.Log("PointEnter is Triggered");
+        animTake_In.SetBool("IsHighlight", true);
+    }
+    public void TakeAnimationExit()
+    {
+        Debug.Log("ExitEnter");
+        animTake_In.SetBool("IsHighlight", false);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public void UseAnimationEnter()
+    {
+        Debug.Log("PointEnter is Triggered");
+        animUse_In.SetBool("IsHighlight", true);
+    }
+    public void UseAnimationExit()
+    {
+        Debug.Log("ExitEnter");
+        animUse_In.SetBool("IsHighlight", false);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public void TalkAnimationEnter()
+    {
+        Debug.Log("PointEnter is Triggered");
+        animTalk_In.SetBool("IsHighlight", true);
+    }
+    public void TalkAnimationExit()
+    {
+        Debug.Log("ExitEnter");
+        animTalk_In.SetBool("IsHighlight", false);
+    }
+}
---
+++ Interaction Panel_ Pop Effect/Assets/PopEffect/Scripts/UIActivate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIActivate : MonoBehaviour
+{
+    public void ActivateTake ()
+    {
+        Debug.Log("Item has been Obtained");
+    }
+
+    public void ActivateUse()
+    {
+        Debug.Log("Item is being use");
+    }
+
+    public void ActivateLook()
+    {
+        Debug.Log("Oh look! Its a useful Item!");
+    }
+
+    public void ActivateSpeak()
+    {
+        Debug.Log("Hello Item! Do you have feelings for me?");
+    }
+}
